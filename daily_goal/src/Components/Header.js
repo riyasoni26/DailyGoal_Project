0d8bd73b7b39898c79c6b8af7f737d97115105e8
riyasoni26 <riyasoni2657@gmail.com>
2026-07-05 13:35:01 +0530

    Updated_Project commit

---
+++ daily_goal/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return <>
+     <nav>Get Ready To Complete Your Daily Goals</nav>
+     </>;
+};
+
+export default Header;                                               
+
